741a3a53d02015613b8b3f04f928f38580b29958
Johnsensei <john@johnsensei.com>
2021-12-08 16:52:26 -0600

    Adds basic social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import '../css/custom.css';
+import { SocialIcon } from 'react-social-icons';
 
 
 
@@ -8,7 +9,9 @@ const Footer = () => {
         <div className='footer'>
           <p style={{color: '#fff', marginTop: '25px'}}>Connect - Contact</p>
           <div>
-
+            <SocialIcon  url='https://github.com/Johnsensei' target='_blank' rel='noreferrer' fgColor='#fff' className='social-button'/>
+            <SocialIcon  url='https://www.linkedin.com/in/johnagale/' target='_blank' rel='noreferrer' fgColor='#fff' className='social-button'/>
+            <SocialIcon  url='https://twitter.com/MrJohnSensei' target='_blank' rel='noreferrer' fgColor='#fff' className='social-button'/>
           </div>
           <p>This site was coded with React, Gatsby, GraphQL, and deployed with AWS Amplify.</p>
           <a>GitHub repo</a>
